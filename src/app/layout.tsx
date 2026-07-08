320f0e2ce82fae5b68619c03f5e456bb34df09e2
leadstonkcom-tech <leadstonk.com@gmail.com>
2026-07-08 13:27:35 +0500

    favicon set

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -126,10 +126,8 @@ export default function RootLayout({
   return (
     <html lang="en" className={`${jakarta.variable} ${bebas.variable}`}>
       <head>
-        <link rel="icon" href="/favicon.ico" sizes="any" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/jpeg" href="/wtechvercefavicon.jpeg" sizes="any" />
+        <link rel="apple-touch-icon" href="/wtechvercefavicon.jpeg" />
         <meta name="theme-color" content="#050505" />
         <meta name="color-scheme" content="dark" />
         <JsonLd />
